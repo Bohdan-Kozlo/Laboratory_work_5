760c0eb0f2c2f82b529786a888a76d610159eb27
Bohdan-Kozlo <119170585+Bohdan-Kozlo@users.noreply.github.com>
2022-11-30 19:58:10 +0200

    Adding file main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Header.h"
+using namespace std;
+
+int main() {
+	Folder1 folder1("folder1");
+	Folder1::Folder2 folder2("folder2");
+	Folder1::Folder2::File file("file1", "pdf", 3);
+	folder1.showPath(folder1,folder2, file);
+	
+}
